d505e46f9be40f2121f70a1981de3e782d163d5b
Ivan Ishchenko <ishchenko.io@yandex.ru>
2016-07-09 22:35:35 +0300

    Исправлен метод для загрузки блоба

--- WebApplication3/Controllers/HomeController.cs
+++ WebApplication3/Controllers/HomeController.cs
@@ -152,32 +152,33 @@ namespace WebApplication3.Controllers
         {
             return View();
         }
-    }
 
-    //The DownloadToStream method was used to download the contents of a blob as a text string
-    public ActionResult downloadBlobs() {
-
-        // Retrieve storage account from connection string.
-        CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
-            CloudConfigurationManager.GetSetting(connectionString));
-
-        // Create the blob client.
-        CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
-
-        // Retrieve reference to a previously created container.
-        CloudBlobContainer container = blobClient.GetContainerReference("mycontainer");
-
-        // Retrieve reference to a blob named "myblob.txt"
-        CloudBlockBlob blockBlob2 = container.GetBlockBlobReference("myblob.txt");
-
-        string text;
-        using (var memoryStream = new MemoryStream())
+        //The DownloadToStream method was used to download the contents of a blob as a text string
+        public ActionResult downloadBlobs()
         {
-            blockBlob2.DownloadToStream(memoryStream);
-            text = System.Text.Encoding.UTF8.GetString(memoryStream.ToArray());
+
+            // Retrieve storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting(connectionString));
+
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Retrieve reference to a previously created container.
+            CloudBlobContainer container = blobClient.GetContainerReference("mycontainer");
+
+            // Retrieve reference to a blob named "myblob.txt"
+            CloudBlockBlob blockBlob2 = container.GetBlockBlobReference("myblob.txt");
+
+            string text;
+            using (var memoryStream = new MemoryStream())
+            {
+                blockBlob2.DownloadToStream(memoryStream);
+                text = System.Text.Encoding.UTF8.GetString(memoryStream.ToArray());
+            }
+
+            return View();
         }
 
-        return View();
     }
-
 }
